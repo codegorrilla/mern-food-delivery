1caa37be5a0e9c101726209aaa854874c87e4435
Sanjib Adhya <code.gorrilla@gmail.com>
2026-05-31 17:27:59 +0530

    developed food items card and food items list

--- front-end/src/components/ExploreMenu/ExploreMenu.jsx
+++ front-end/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="explore-menu" id="explore-menu">
-      <h1>Explore Our Mdenu</h1>
+      <h1>Explore Our Menu</h1>
       <p className="explore-menu-text">
         Choose from a diverse menu featuring a delectable array of dishes
         crafted with the finest ingradients and culinary expertise. Our mission
@@ -14,8 +14,20 @@ const ExploreMenu = () => {
       <div className="explore-menu-list">
         {menu_list.map((item, index) => {
           return (
-            <div key={index} className="explore-menu-list-item">
-              <img src={item.menu_image} alt="" />
+            <div
+              key={index}
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name,
+                )
+              }
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt=""
+              />
               <p>{item.menu_name}</p>
             </div>
           );
