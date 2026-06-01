66d5fc6a4c797ce1aae21dcfa912574b1997f26c
Sanjib Adhya <code.gorrilla@gmail.com>
2026-06-01 14:43:24 +0530

    completed development for the cart, place order page, login pop-up component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <BrowserRouter>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
         </Routes>
       </div>
+      <Footer />
     </BrowserRouter>
   );
 };
